d3e05db1063ef9633600756598c4e30752810eb4
AATHMIGA S <aathmigaads@gmail.com>
2026-03-31 20:46:39 +0530

    Update config_adapter.py

--- config_adapter.py
+++ config_adapter.py
@@ -1,11 +1,11 @@
 def adapt_to_existing_format(user_config):
-    """
-    Convert user input into format expected by pipeline
-    """
+
+    services = user_config["services"]
 
     manifest = {
         "name": user_config["application"],
-        "runAsNonRoot": False   # intentionally unsafe to trigger repair
+        "runAsNonRoot": False,
+        "services": services   # ✅ ADD THIS
     }
 
     return manifest
