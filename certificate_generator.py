f60f5bb696c7979168592abd4947436824c9750d
AATHMIGA S <aathmigaads@gmail.com>
2026-03-31 21:29:14 +0530

    Create certificate_generator.py

---
+++ certificate_generator.py
@@ -0,0 +1,40 @@
+import os
+from datetime import datetime
+
+
+def generate_certificate(result):
+
+    # Ensure folder exists
+    os.makedirs("certificates", exist_ok=True)
+
+    app_name = result['after']['name']
+    filename = f"certificates/cert_{app_name}.txt"
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    content = f"""
+========================================
+   KUBERNETES SECURITY CERTIFICATE
+========================================
+
+Application Name : {app_name}
+Status           : {'SECURE' if result['verified'] else 'UNSAFE'}
+
+Violations Found : {result['violation']}
+Repairs Applied  : {result['repairs']}
+
+Certificate Hash : {result['certificate']}
+
+Generated At     : {now}
+
+========================================
+Certified by Neuro-Symbolic Engine
+========================================
+"""
+
+    with open(filename, "w") as f:
+        f.write(content)
+
+    print(f"\n📄 Certificate saved: {filename}")
+
+    return filename
