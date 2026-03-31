b2a45f4f75d206a85b37cd3378ad1110c47cd09b
AATHMIGA S <aathmigaads@gmail.com>
2026-03-31 21:38:57 +0530

    Update run_with_input.py

--- run_with_input.py
+++ run_with_input.py
@@ -2,6 +2,7 @@ from input_handler import get_user_input
 from config_adapter import adapt_to_existing_format
 from experiments.proposed.neuro_symbolic_pipeline import run
 from k8s_generator import generate_k8s_yaml, save_yaml
+from certificate_generator import generate_certificate
 
 def main():
     # Step 1: Input
@@ -56,6 +57,12 @@ def main():
 
     k8s_objects = generate_k8s_yaml(user_input)
     save_yaml(k8s_objects)
+
+    cert_file = generate_certificate(result)
+
+    print("\n📥 DOWNLOAD CERTIFICATE")
+    print("----------------------------------")
+    print(f"Path: {cert_file}")
 
 if __name__ == "__main__":
     main()
